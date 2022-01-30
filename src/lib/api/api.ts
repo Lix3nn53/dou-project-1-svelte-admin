4eb24ced12379b0d2b033980619d72f5f956b7cf
Lix3nn53 <lixakyildiz@gmail.com>
2022-01-30 20:31:18 +0300

    Work on replacing axios with fetch api

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -1,75 +1,87 @@
 import axios from 'axios';
 import TokenService from './TokenService';
 
-const baseURL = 'http://localhost:8080/v1';
+const base = 'http://localhost:8080/v1';
 
-const instance = axios.create({
-  baseURL,
-});
+async function refreshAccessToken() {
+  const token = TokenService.getLocalRefreshToken();
 
-instance.interceptors.request.use(
-  (config) => {
-    if (config.url === '/auth/logout') {
-      const token = TokenService.getLocalRefreshToken();
-      if (token) {
-        /* eslint-disable-next-line no-param-reassign */
-        config.headers.Authorization = `Bearer ${token}`; // for Spring Boot back-end
-        // config.headers['x-access-token'] = token; // for Node.js Express back-end
-      }
+  if (!token) {
+    return Promise.reject(new Error('no local refresh token'));
+  }
 
-      return config;
+  try {
+    const rs = await axios.get(`${base}/auth/refresh_token`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const save = TokenService.isSaved();
+    TokenService.setLocalAccessToken(rs.data.access_token, save);
+
+    return true;
+  } catch (_error) {
+    return Promise.reject(_error);
+  }
+}
+
+async function send({ method, path, data }) {
+	const opts: any = { method, headers: {} };
+
+	if (data) {
+		opts.headers['Content-Type'] = 'application/json';
+		opts.body = JSON.stringify(data);
+	}
+
+
+  if (path === '/auth/logout') { // only path that uses refresh token
+    const token = TokenService.getLocalRefreshToken();
+    if (token) {
+      opts.headers['Authorization'] = `Bearer ${token}`;
     }
-
+  } else {
     const token = TokenService.getLocalAccessToken();
     if (token) {
-      /* eslint-disable-next-line no-param-reassign */
-      config.headers.Authorization = `Bearer ${token}`; // for Spring Boot back-end
-      // config.headers['x-access-token'] = token; // for Node.js Express back-end
+      opts.headers['Authorization'] = `Bearer ${token}`;
     }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
+  }
 
-instance.interceptors.response.use(
-  (res) => {
-    return res;
-  },
-  async (err) => {
-    const originalConfig = err.config;
+	return fetch(`${base}${path}`, opts)
+		.then((r) => r.text())
+		.then((json) => {
+			try {
+				return JSON.parse(json);
+			} catch (err) {
+				return json;
+			}
+		});
+}
 
-    if (err.response) {
-      // Access Token was expired
-      if (err.response.status === 401 && !originalConfig.retried) {
-        originalConfig.retried = true;
+export async function sendWithRefresh({ method, path, data }) {
+	const response = await send({ method, path, data });
 
-        const token = TokenService.getLocalRefreshToken();
+  if (response.status === 401) {
+    const refresh = await refreshAccessToken();
 
-        if (!token) {
-          return Promise.reject(new Error('no local refresh token'));
-        }
-
-        try {
-          const rs = await axios.get(`${baseURL}/auth/refresh_token`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-
-          const save = TokenService.isSaved();
-          TokenService.setLocalAccessToken(rs.data.access_token, save);
-
-          return instance(originalConfig);
-        } catch (_error) {
-          return Promise.reject(_error);
-        }
-      }
+    if (refresh) { // try again if refresh
+      return await send({ method, path, data });
     }
+  }
+}
 
-    return Promise.reject(err);
-  },
-);
+export function get(path) {
+	return sendWithRefresh({ method: 'GET', path, data: null });
+}
 
-export default instance;
+export function del(path) {
+	return send({ method: 'DELETE', path, data: null });
+}
+
+export function post(path, data) {
+	return send({ method: 'POST', path, data });
+}
+
+export function put(path, data) {
+	return send({ method: 'PUT', path, data });
+}
