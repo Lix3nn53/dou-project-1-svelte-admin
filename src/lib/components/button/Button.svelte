84565356c72532690d5d1c0930a3b631ed921d35
Lix3nn53 <lixakyildiz@gmail.com>
2022-01-30 12:12:26 +0300

    Form button disabled state

--- src/lib/components/button/Button.svelte
+++ src/lib/components/button/Button.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
 	export let type: 'button' | 'menu' | 'reset' | 'submit';
+	export let disabled: boolean;
 </script>
 
 <button
 	on:click
-	class="shadow bg-indigo-600 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-6 rounded"
+	{disabled}
+	class="shadow bg-indigo-600 hover:bg-indigo-400 
+	focus:shadow-outline focus:outline-none 
+	text-white font-bold py-2 px-6 rounded 
+	disabled:cursor-not-allowed disabled:bg-base-400"
 	{type}
 >
 	<slot />
--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,27 +1,63 @@
 <script lang="ts">
+	// svelte imports
+	import type { Writable } from 'svelte/store';
+	// form imports
 	import { createForm } from 'felte';
-	import { svelteReporter as reporter, ValidationMessage } from '@felte/reporter-svelte';
+	import { svelteReporter as reporter } from '@felte/reporter-svelte';
 	import { validator } from '@felte/validator-yup';
 	import * as yup from 'yup';
+	// component imports
 	import Fieldset from '$lib/components/form/Fieldset.svelte';
 	import Field from '$lib/components/form/Field.svelte';
 	import Button from '$lib/components/button/Button.svelte';
-	import Wave from '$lib/components/divider/DividerWave.svelte';
+	// api imports
+	import AuthAPI from '$lib/api/AuthAPI';
 
 	const schema = yup.object({
 		IDNumber: yup.string().length(11).required('Required'),
 		password: yup.string().min(2, 'Too Short!').max(30, 'Too Long!').required('Required'),
 	});
 
+	const delay = ms => new Promise(res => setTimeout(res, ms));
+
+	async function handleSubmit(
+    values: Record<string, string>,
+    isSubmitting: Writable<boolean>,
+  ): Promise<boolean> {
+    // alert(JSON.stringify(values, null, 2));
+    isSubmitting.set(true);
+
+	await delay(5000);
+
+    const remember = JSON.parse(values.remember);
+    const res = await AuthAPI.login(values.IDNumber, values.password, remember);
+
+    if (res.error) {
+      alert(`error: ${res.error}`);
+      isSubmitting.set(false);
+      return false;
+    }
+
+    isSubmitting.set(false);
+    window.location.href = '/';
+    return true;
+  }
+
 	// Creating the form
-	const { form, data } = createForm<yup.InferType<typeof schema>>({
+	const { form, data, isSubmitting } = createForm<yup.InferType<typeof schema>>({
 		initialValues: {
 			IDNumber: '',
 			password: ''
 		},
-		onSubmit: (values) => alert(values),
+		onSubmit: (values) => handleSubmit(values, isSubmitting),
 		extend: [validator, reporter],
 		validateSchema: schema
+	});
+
+let isSubmittingValue;
+
+isSubmitting.subscribe(value => {
+	isSubmittingValue = value;
 	});
 </script>
 
@@ -47,7 +83,7 @@
 					<Field id="remember" label="Remember Me" type="checkbox" />
 				</Fieldset>
 				<span class="self-end">
-					<Button type="submit">Login!</Button>
+					<Button type="submit" disabled={isSubmittingValue}>Login!</Button>
 				</span>
 			</form>
 
